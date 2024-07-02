b181463de8cb9262e680493c7c20ae2bf985073d
PJMonticoli <pablo_monticoli@hotmail.com>
2024-07-02 10:30:45 -0300

    agrego componente DwTable y pagination

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <nav class="navbar">
+    <ul>
+      <li>
+        <span class="nav-title">Recepciones</span>
+        <img src="../assets/images/icons8-home-24.png" width="20px">
+        <span>- Planificación</span><span class="enabled">- Recepciones</span>
+      </li>
+      <li>
+        <button class="button">Nuevo</button>
+        <button class="button">Importar</button>
+      </li>
+    </ul>
+  </nav>
+</template>
