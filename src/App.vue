0f1257cb2902b486874578d5ebbdd03110b35113
PJMonticoli <pablo_monticoli@hotmail.com>
2024-07-02 10:30:10 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterView } from 'vue-router'
+
+import HeaderBar from './components/HeaderBar.vue'
+import Sidebar from '@/components/Sidebar.vue'
+import NavigationBar from './components/NavigationBar.vue'
+</script>
+
+<template>
+  <Sidebar />
+  <div class="w-100">
+    <div class="headers w-100">
+      <HeaderBar />
+      <div class="line"></div>
+      <NavigationBar />
+    </div>
+    <div class="container">
+      <h1>Hola</h1>
+      <RouterView />
+    </div>
+  </div>
+</template>
---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <div class="headerbar">
+        <div class="icons">
+            <img id="configuration" src="../assets/images/icons8-ajustes-24.png" width="20">
+            <img id="notification" src="../assets/images/icons8-notification-24.png" width="20">
+            <img id="user-logo" src="../assets/images/icons8-usuario-30.png">
+        </div>
+        <p id="user-name">User name</p>
+    </div>
+</template>
